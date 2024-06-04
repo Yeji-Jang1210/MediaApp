72590177287bdd1144c939140a3d046c80fdabc2
Yeji-Jang1210 <jyg8727@gmail.com>
2024-06-04 16:13:09 +0900

    Initial Commit

---
+++ MediaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaApp
+//
+//  Created by 장예지 on 6/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
